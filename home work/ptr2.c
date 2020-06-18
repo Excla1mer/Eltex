c8c57e041a2cb1cbc95cd1e97e615841e3293ebe
Sergey <S_samoilenko_99@mail.ru>
2020-06-18 16:55:57 +0700

    HW pointer #2

---
+++ home work/ptr2.c
@@ -0,0 +1,17 @@
+#include  <stdio.h>
+
+
+int main() {
+	char a = 'A';
+	char b = 'B';
+	char *ptr1;
+	char **ptr2;
+
+	ptr1 = &a;
+	printf("*ptr1 = %c\n", *ptr1);
+	ptr2  = &ptr1;
+
+	*ptr2 = &b;
+	printf("*ptr1 = %c\n", *ptr1);
+	return 0;
+}
