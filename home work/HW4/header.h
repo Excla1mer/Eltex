a5c9228657f64c4ddb3a84851a9eebd44aa47a6c
Sergey <S_samoilenko_99@mail.ru>
2020-07-02 15:57:20 +0700

    Dynamic memory for my list

--- home work/HW4/header.h
+++ home work/HW4/header.h
@@ -3,14 +3,15 @@
 
 #include <stdio.h>
 #include <string.h>
-
+#include <malloc.h>
+#include <alloca.h>
 struct test {
 	char a;
 	int b;
 };
 
 struct book {
-	char Name[10];
+	char Name[16];
 	unsigned long int Number;
 };
 
--- home work/HW4/struct2.c
+++ home work/HW4/struct2.c
@@ -1,33 +1,34 @@
 #include "header.h"
-#define N 5
 
 int main() {
-	struct book list[N] = {{{'S', 'e', 'r', 'g', 'e', 'y'}, 88005553535},
-				{{'D', 'i','m', 's'}, 654862135}};
+
+	struct book *list = malloc(sizeof(struct book));;
 	char a;
-	int b;
+	int Quantity = 0;
 	char str[255];
 	int find = 0;
+
 	do {
 		printf("\t---------Menu-------\n");
 		printf("\t|  [1] View list   |\n");
 		printf("\t|  [2] Add user    |\n");
-		printf("\t|  {3} Find user   |\n");
+		printf("\t|  [3] Find user   |\n");
 		printf("\t|  [4] Remove user |\n");
+		printf("\t|  [5] Exit        |\n");
 		printf("\t--------------------\n");
 		scanf("%c", &a);
 		//fgets(&a, 2, stdin);
 		switch(a) {
 			//Viviod vsego spiska
 			case '1':
-				for(int i = 0; i < N; i++) {
+				for(int i = 0; i < Quantity; i++) {
 					if(list[i].Number  != 0)
 						printf("Name: %s Number: %ld\n", list[i].Name, list[i].Number);
 				}
 				break;
 			//Dobavleni novogo usera v spisok
 			case '2':
-				for(int i  = 0; i < N; i++) {
+				for(int i  = 0; i < Quantity; i++) {
 					if(list[i].Number == 0) {
 						printf("Enter name:");
 						scanf("%s", list[i].Name);
@@ -35,12 +36,34 @@ int main() {
 						scanf("%ld", &list[i].Number);
 						break;
 					}
+					//If no free space
+					if( i == (Quantity - 1) )
+					{
+						list = realloc(list, (sizeof(struct book) * (Quantity + 1)));
+						printf("Enter name:");
+                                                scanf("%s", list[Quantity].Name);
+                                                printf("Enter Number: ");
+                                                scanf("%ld", &list[Quantity].Number);
+						Quantity++;
+						break;
+					}
+				}
+				//If list empty
+				if ( Quantity == 0 )
+				{
+					Quantity++;
+					printf("Enter name:");
+                                        scanf("%s", list[0].Name);
+                                        printf("Enter Number: ");
+                                        scanf("%ld", &list[0].Number);
+
 				}
                                 break;
+			//Find user
                        case '3':
                        	printf("Enter Name:");
 				scanf("%s", str);
-				for(int i = 0; i < N; i++) {
+				for(int i = 0; i < Quantity; i++) {
 					if(strcmp(str, list[i].Name) == 0) {
 						find++;
 						printf("Name: %s Number: %ld\n", list[i].Name, list[i].Number);
@@ -53,7 +76,7 @@ int main() {
 					find = 0;
 				}
                        	break;
-                       //Ydalenie usera po imeni
+                       //Delete user by name
 			case '4':
 				printf("Enter Name:");
 				scanf("%s", str);
@@ -65,6 +88,10 @@ int main() {
 					}
 				}
                                 break;
+			//Exit
+			case '5':
+				_Exit(0);
+				break;
 			default :
 				break;
 		}
