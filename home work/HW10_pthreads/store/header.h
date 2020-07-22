68766396e78a4206a64d5bb8aec81a3367131ff8
Sergey <S_samoilenko_99@mail.ru>
2020-07-22 14:37:43 +0700

    remove pthreads functions to another file

--- home work/HW10_pthreads/store/header.h
+++ home work/HW10_pthreads/store/header.h
@@ -1,5 +1,14 @@
+#ifndef __HEADER_H_
+#define __HEADER_H_ 5
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+
+int store[5];
+
+void *customer(void *param);
+void *worker(void *param);
+
+#endif
