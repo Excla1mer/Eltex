6e0f0240f1c8de6b319f226e44ca26be327a0c49
Sergey <S_samoilenko_99@mail.ru>
2020-07-22 14:10:10 +0700

    22.07

--- home work/HW10_pthreads/main.c
+++ home work/HW10_pthreads/main.c
@@ -1,20 +1,23 @@
 #include "header.h"
 long sum = 0;
+pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
 void *func(void *param) {
 	long tmp;
 	long *ptr;
 	ptr = (long *)param;
 	for(long j = 0; j < 10000000; j++) {
-		tmp = *ptr;
-		tmp++;
-		*ptr = tmp;
+		pthread_mutex_lock(&mut);
+		//tmp = *ptr;
+		//tmp++;
+		//*ptr = tmp;
+		*ptr = *ptr + 1;	
+		pthread_mutex_unlock(&mut);
 	}
 	return 0;
 }
 
 int main() {
 	pthread_t tid[500];
-
 	for(int i = 0; i < 500; i++) {
 		pthread_create(&tid[i], NULL, func, &sum);
 	}
