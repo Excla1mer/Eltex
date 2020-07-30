d356dcad1580e70a713256bd10f12a921ed140af
Sergey <S_samoilenko_99@mail.ru>
2020-07-30 13:06:21 +0700

    полностью рабочая логика обмена сообщениями

--- home work/HW12_chat/client.c
+++ home work/HW12_chat/client.c
@@ -1,39 +1,69 @@
 #include "header.h"
 
-#define NAME_SIGN "/signals01"
+#define NAME_SIGN "/signals0"
 #define NAME_MSG "/messages1"
 
 unsigned int count_u;
 unsigned int count_m;
 
+unsigned int sgn_prio_send = 2;
+unsigned int sgn_prio_receive = 1;
+mqd_t client;
+void* clients_msg(void *param) {
+	char **msg_buff = (char **)param;
+	while(1) {
+		msg_buff[count_m] = malloc(80);
+		printf("wait msg:\n");
+		if(mq_receive(client, msg_buff[count_m], 80, NULL) == -1) {
+                        perror("MSG mq_receive");
+                        exit(1);
+                } else count_m++;
+		printf("Got: %s\n", msg_buff[count_m-1]);
+		msg_buff[count_m] = malloc(80);
+	}
+}
+
 int main(int argc, char *argv[]) {
 
 	mqd_t msg;
 	mqd_t sign;
-	mqd_t client;
+	//mqd_t client;
 	char** msg_buff = malloc(sizeof(char*));
 	char msg_b[80];
 	char  client_ch[80] = "/";
-	msg_buff[count_m] = malloc(80);
+	pthread_t tid_msg;
+	//msg_buff[count_m] = malloc(80);
 	strcat(client_ch, argv[1]);
 	if((msg = mq_open(NAME_MSG, O_RDWR)) == -1) {
                 perror("MSG mq_open:");
                 exit(1);
-        }	
+        }
+        printf("open msg\n");	
 	if((sign = mq_open(NAME_SIGN, O_RDWR)) == -1) {
                 perror("SIGN mq_open:");
                 exit(1);
         }
-	if(mq_send(sign, argv[1], strlen(argv[1]) + 1, 0) == -1) {
+        printf("open sign\n");
+	if(mq_send(sign, argv[1], strlen(argv[1]) + 1, sgn_prio_send) == -1) {
 		perror("SIGN mq_send");
 		exit(1);
 	}
-	sleep(1);
+	printf("sended sign\n");
+	if(mq_receive(sign, msg_b, 80, &sgn_prio_receive) == -1) {
+                        perror("SIGN mq_receive");
+                        exit(1);
+    	}
+    	printf("got sign %s\n", msg_b);
+    	if(strcmp(msg_b, argv[1]) != 0) {
+    		perror("Strcmp :");
+    		exit(1);
+    	} 
+	//sleep(1);
 	if((client = mq_open(client_ch, O_RDWR)) == -1) {
                 perror("client mq_open:");
                 exit(1);
         }
-
+	pthread_create(&tid_msg, NULL, clients_msg, msg_buff);
 	while(1) {
 		printf("input  msg: ");
 		fgets(msg_b, 80, stdin);
@@ -41,13 +71,12 @@ int main(int argc, char *argv[]) {
                 	perror("SIGN mq_send");
                 	exit(1);
         	}
-		if(mq_receive(client, msg_buff[count_m], 80, NULL) == -1) {
+		/*if(mq_receive(client, msg_buff[count_m], 80, NULL) == -1) {
                         perror("MSG mq_receive");
                         exit(1);
                 } else count_m++;
 		printf("Got: %s\n", msg_buff[count_m-1]);
-		
-		
+		msg_buff[count_m] = malloc(80);*/	
 	}
 
 	return 0;
--- home work/HW12_chat/server.c
+++ home work/HW12_chat/server.c
@@ -1,12 +1,15 @@
 #include "header.h"
 
-#define NAME_SIGN "/signals01"
+#define NAME_SIGN "/signals0"
 #define NAME_MSG "/messages1"
 #define MAX_MSG 10
 #define MAX_MSG_SIZE 80
 
 unsigned int count_u; // count of users
 unsigned int count_m; // count of messages 
+
+unsigned int sgn_prio_send = 1;
+unsigned int sgn_prio_receive = 2;
 
 mqd_t client[10];
 void* signals(void *param) {
@@ -19,31 +22,32 @@ void* signals(void *param) {
 	attr.mq_maxmsg = MAX_MSG;
 	attr.mq_msgsize = MAX_MSG_SIZE;
 	attr.mq_curmsgs = 0;
-	if((sign = mq_open(NAME_SIGN, O_RDONLY | O_CREAT, 0777, &attr)) == -1) {
+	printf("open sign\n");
+	if((sign = mq_open(NAME_SIGN, O_RDWR | O_CREAT, 0777, &attr)) == -1) {
 		perror("SIGN mq_open:");
 		exit(1);
 	}
 	users[0] = malloc(80);
 	while(1) {
-		if(mq_receive(sign, users[count_u], attr.mq_msgsize, NULL) == -1) {
+		users[count_u] = malloc(80);
+		printf("waiting sign\n");
+		if(mq_receive(sign, users[count_u], attr.mq_msgsize, &sgn_prio_receive) == -1) {
 			perror("SIGN mq_receive");
                         exit(1);
                 }
 		//client[count_u] = malloc(sizeof(mqd_t));
                 strcat(client_ch, users[count_u]);
-		users[count_u] = malloc(80);
 		if((client[count_u] = mq_open(client_ch, O_WRONLY | O_CREAT, 0777, &attr)) == -1) {
                 	perror("SIGN mq_open:");
                 	exit(1);
         	}
-
-		/*for(int i = 0; i < count; i++){
-			if(mq_send(client_ch, users[i], attr.mq_msgsize, NULL) == -1) {
+        	if(mq_send(sign, users[count_u], attr.mq_msgsize, sgn_prio_send) == -1) {
                         	perror("SIGN mq_receive");
                         	exit(1);
-                	}
-
-		}*/
+                }
+                printf("sended sign %s\n", users[count_u]);
+		memset(client_ch, 0, 80);
+		strcat(client_ch, "/");
 		count_u++;
 	}
 
@@ -66,18 +70,21 @@ int main()  {
                 perror("MSG mq_open:");
                 exit(1);
         }
+        printf("msg open\n");
 	pthread_create(&tid_sign, NULL, signals, u_names);
 	while(1) {
+		printf("waiting msg\n");
 		if(mq_receive(msg, msg_buff[count_m], attr.mq_msgsize, NULL) == -1) {
 			perror("MSG mq_receive");
 			exit(1);
 		} else count_m++;
-
+		msg_buff[count_m] = malloc(80);
 		for(int i = 0; i < count_u; i++) {
 			if(mq_send(client[i], msg_buff[count_m-1], attr.mq_msgsize, 0) == -1) {
                                 perror("SIGN mq_receive");
                                 exit(1);
                         }
+                        printf("sended msg %s\n", msg_buff[count_m-1]);
 			/*memset(client_ch, 0, sizeof(client_ch));
 			strcat(client_ch, "/");*/
 		}
