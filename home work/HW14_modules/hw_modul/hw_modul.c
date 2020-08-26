695844a407babd44a90d18558c6325c02c7549fe
Sergey <s_samoilenko_99@mail.ru>
2020-08-26 10:31:54 +0700

    добавил обработку записи в модуль

--- home work/HW14_modules/hw_modul/hw_modul.c
+++ home work/HW14_modules/hw_modul/hw_modul.c
@@ -11,20 +11,21 @@ static ssize_t my_write(struct file *, const char *, size_t, loff_t *);
 #define DEVICE_NAME "my_module"
 
 static int Major;
+static char msg_buff[80];
 
-static ssize_t my_read(struct file *filp, /* см. include/linux/fs.h   */
-         char *buffer,                        /* буфер, куда надо положить данные */
-         size_t length,                       /* размер буфера */
-         loff_t * offset)
-{
+static ssize_t my_read(struct file *filp, char *buffer, size_t length, loff_t * offset) {
 	printk(KERN_INFO "You What read my_modul\n");
 	return 0;
 }
 
-static ssize_t my_write(struct file *filp, const char *buff, size_t len, loff_t * off)
+static ssize_t my_write(struct file *filp, const char *user_buff, size_t len, loff_t * off)
 {
-	printk(KERN_INFO "You whan write my_modul\n");
-	return 0;
+	int ret;
+	ret = simple_write_to_buffer(msg_buff, sizeof(msg_buff), off, user_buff, len);
+	if (!ret)
+		return -EFAULT;
+	printk(KERN_INFO "You whan write my_modul: %s\n", msg_buff);
+	return ret < 0 ? ret : len;
 }
 
 static struct file_operations fops = {
